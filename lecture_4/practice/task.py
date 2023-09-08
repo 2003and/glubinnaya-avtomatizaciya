3f12724a06f0fdc1a99d2e82793eb2298f37c50b
Andrew Pavin <2003and@gmail.com>
2023-09-08 23:35:40 +0300

    lecture 4 task added commentary and typehinting

--- lecture_4/practice/task.py
+++ lecture_4/practice/task.py
@@ -1,27 +1,32 @@
-from import_this import generate_race_data
+from import_this import generate_race_data, RaceInfo
 from time_conversion import seconds_conversion
 
 
 if __name__  == "__main__":
-    generated_race_data = generate_race_data(5)
+    # generate random input data
+    generated_race_data: RaceInfo = generate_race_data(5)
+
     # empty winner description string declaration
     winner: str = ""
+
+    # generating winner description string contents
     for i in generated_race_data.values():
         # finding the first-placed racer
         if i.get("FinishedPlace", -1) == 1:
             winner = f"Выиграл - {i.get('RacerName','лох какойто)').upper()}!!! Поздравляем!"
             break
-    # winner += "\n"+
-    print(winner)
-    print("_"*len(winner))
+    print(winner) # printing winner description
+    print("_"*len(winner)) # printing separator
 
+    # Describing first three places
     print("\nПервые три места:\n")
     for curr_racist in range(1, 4):
-        racist_desc = f"\tГонщик на {'первом' if curr_racist == 1 else 'втором' if curr_racist == 2 else 'третьем'} месте:\n"
+        racist_desc: str = f"\tГонщик на {'первом' if curr_racist == 1 else 'втором' if curr_racist == 2 else 'третьем'} месте:\n"
+        # finding the [curr_racist]-placed racer
         for i in generated_race_data.values():
             if i.get("FinishedPlace",-1) == curr_racist:
-                racist_desc += f"\t\tИмя:{i.get('RacerName','лох какойто)')}\n"
-                racist_desc += f"\t\tКоманда:{i.get('RacerTeam','лохи какието)')}\n"
-                racist_time = seconds_conversion(i.get('FinishedTimeSeconds',0))
-                racist_desc += f"\t\tВремя:{racist_time[0]}:{racist_time[1]}:{racist_time[2]}\n"
-        print(racist_desc)  
+                racist_desc       += f"\t\tИмя:{i.get('RacerName','лох какойто)')}\n"                  # adding racer name to desc
+                racist_desc       += f"\t\tКоманда:{i.get('RacerTeam','лохи какието)')}\n"             # adding team name to desc
+                racist_time: tuple = seconds_conversion(i.get('FinishedTimeSeconds',0))                # processing time
+                racist_desc       += f"\t\tВремя:{racist_time[0]}:{racist_time[1]}:{racist_time[2]}\n" # adding timestamp to desc
+        print(racist_desc) # printing racer description
